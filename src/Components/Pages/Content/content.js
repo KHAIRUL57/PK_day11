4ee6ae80d759e6001c84f23bdb3857bacbe993c1
KHAIRUL57 <khairultamimi5757@gmail.com>
2020-08-29 16:36:02 +0700

    learn to create react

---
+++ src/Components/Pages/Content/content.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../../../Components/Button/Button";
+
+const content = () => {
+  return (
+    <div>
+      <h1>Hello Word</h1>
+      <Button text="tidak ada" />
+    </div>
+  );
+};
+
+export default content;
